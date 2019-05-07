cba3c92a983a483e0b3fac12db331c0d68d567ce
John <dobiedevelopments@gmail.com>
2019-05-07 01:24:36 -0400

    updated react app source

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World Website !
+          dobiedevelopments.github.io
         </a>
       </header>
     </div>
