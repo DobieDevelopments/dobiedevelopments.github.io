12c6a9b6e166700a577bafbdee501f29e1ed4e86
John <dobiedevelopments@gmail.com>
2019-05-07 03:43:25 -0400

    added clarification
    unity game is a test, no actual game is there yet.

---
+++ precache-manifest.709ca8103b009203fa8f149fece0815e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6528a4fb312254d076868605541e891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aacdb706396149313d1",
+    "url": "/static/css/main.30bab35e.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "9aacdb706396149313d1",
+    "url": "/static/js/main.687bcc78.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
