ae885c2655067c741d3f7132f6eda674bfe5a7a1
John <dobiedevelopments@gmail.com>
2019-05-07 03:39:21 -0400

    added test unity link, modified overall look a bit
    purple and green

---
+++ build/precache-manifest.b6aad4dca59e8b5adc2550fc3063c213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf8d37e3b5ac9016e348be503782c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db5333cb9e71b5019338",
+    "url": "/static/css/main.30bab35e.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "db5333cb9e71b5019338",
+    "url": "/static/js/main.491f21da.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,23 +7,45 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <code>TODO:</code>
-        design website <br />
+      </header>
+      <div className="Header-info">
+        TODO:
+        <br />
+        think of cool design for website <br />
+        implement the cool web design things <br />
         fix auto-deployment for gh-pages
         <br /> ???
-        <br /> profit.
+        <br /> treat self to a nice dinner. 🍉
         <br />
+      </div>
+      <div className="Green">
         <br />
-        <a
-          className="App-link"
-          href="https://github.com/DobieDevelopments/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          check out my GitHub!
-        </a>{" "}
-        (:
-      </header>
+      </div>
+      <div className="App-content">
+        <p>
+          {"Go back to GitHub: "}
+          <a
+            className="App-link"
+            href="https://github.com/DobieDevelopments/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            click here!
+          </a>
+        </p>
+        <p>
+          {"Unity: "}
+          <a
+            className="App-link"
+            href="https://DobieDevelopments.github.io/popgame/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            pop pop game
+          </a>
+          {" by John Dobie."}
+        </p>
+      </div>
     </div>
   );
 }
